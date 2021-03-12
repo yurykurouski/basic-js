152881d97b7c3fe92f146c6ee8968379b8f2eaf0
Yury <ymorpheus@gmail.com>
2021-03-12 15:16:36 +0300

    feat: add What season? sub-task solution

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+  if (!(date instanceof Date)) throw Error;
+
+  const year = {
+    winter: ['Jan', 'Feb', 'Dec'],
+    spring: ['Mar', 'Apr', 'May'],
+    summer: ['Jun', 'Jul', 'Aug'],
+    autumn: ['Sep', 'Oct', 'Nov']
+  }
+
+  const month = date.toGMTString().split(' ')[2];
+
+  for (let key in year) {
+    if (year[key].includes(month)) return key
+  }
 };
