042d5fef2a59d74c4d1108727787c2a4dc587b65
Yury <ymorpheus@gmail.com>
2021-03-29 02:48:14 +0300

    feat: implement "recursive-depth" sub-task solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,6 +2,12 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
+    for (let el of arr) {
+      if (Array.isArray(el)) {
+        return this.calculateDepth(arr.flat()) + 1;
+      }
+    }
 
+    return 1;
   }
 };
