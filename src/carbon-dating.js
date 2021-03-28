c38435c49eb2d2441efe18d472fbdb298381eb78
Yury <ymorpheus@gmail.com>
2021-03-29 00:16:56 +0300

    feat: add 'carbon-dating' solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  const numerized = Number(sampleActivity);
+
+  if (!numerized || numerized <= 0 || typeof sampleActivity !== 'string') return false;
+
+  return numerized > MODERN_ACTIVITY ? false : Math.ceil((Math.log(MODERN_ACTIVITY / sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
 };
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+
   }
 };
