27ff11e53f2929ffce49ee4792ab1dc218e02570
Yury <ymorpheus@gmail.com>
2021-03-29 14:06:47 +0300

    feat: add "extended-repeater" sub-taskl solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  const { repeatTimes = 1, separator = "+", addition = "", additionRepeatTimes = 1, additionSeparator = "|" } = options;
+  let additionRes = "";
+
+  for (let i = 1; i < additionRepeatTimes; i++) {
+    additionRes += addition + additionSeparator;
+  }
+
+  let result = str + additionRes + addition;
+  let arr = [];
+
+  for (let i = 1; i <= repeatTimes; i++) {
+    arr.push(result);
+  }
+
+  return arr.join(separator);
 };
-  
